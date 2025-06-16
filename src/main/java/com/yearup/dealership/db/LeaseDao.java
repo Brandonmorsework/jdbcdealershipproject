0a5e48fb1ef7faf8c1df8a0627edb2bb562ea8bb
Brandonmorsework <brandonmorsework@gmail.com>
2025-06-16 19:56:36 -0400

    fixed addLeaseContract method logic

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -4,6 +4,7 @@ import com.yearup.dealership.models.LeaseContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -17,13 +18,18 @@ public class LeaseDao {
     public void addLeaseContract(LeaseContract leaseContract) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(
-                     "Insert into lease_contract (contract_id, vin, lease_start, lease_end, monthly_payment) values (?)")) {
-           /* preparedStatement.setLeaseContract(1, leaseContract);*/
+                     "Insert into lease_contract (contract_id, vin, lease_start, lease_end, monthly_payment) values (?, ?, ?, ?, ?)")) {
+             preparedStatement.setInt(1, leaseContract.getContractId());
+             preparedStatement.setString(2, leaseContract.getVin());
+             preparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseStart()));
+             preparedStatement.setDate(4, Date.valueOf(leaseContract.getLeaseEnd()));
+             preparedStatement.setDouble(5, leaseContract.getMonthlyPayment());
 
 
             int entries = preparedStatement.executeUpdate();
 
             System.out.println("Entries Updated: " + entries);
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -1,9 +1,11 @@
 package com.yearup.dealership.db;
 
+import com.yearup.dealership.models.LeaseContract;
 import com.yearup.dealership.models.SalesContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -15,6 +17,24 @@ public class SalesDao {
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(
+                         "Insert into lease_contract (contract_id, vin, lease_start, lease_end, monthly_payment) values (?, ?, ?, ?, ?)")) {
+                preparedStatement.setInt(1, leaseContract.getContractId());
+                preparedStatement.setString(2, sa.getVin());
+                preparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseStart()));
+                preparedStatement.setDate(4, Date.valueOf(leaseContract.getLeaseEnd()));
+                preparedStatement.setDouble(5, leaseContract.getMonthlyPayment());
+
+
+                int entries = preparedStatement.executeUpdate();
+
+                System.out.println("Entries Updated: " + entries);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
